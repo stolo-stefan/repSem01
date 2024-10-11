bbbbba262004e41b99fdd7252875d3c44be99ce8
sollow <stefand.stoleriu@gmail.com>
2024-10-11 15:58:21 +0300

    added ce am fct la sem2

--- simpleServer/ex4.js
+++ simpleServer/ex4.js
@@ -11,3 +11,20 @@ function addToArray(arr,...args){
 let a=[1]
 
 console.log(addToArray(a,2,3,4))
+
+function merge(first,second){
+    if(first.length!==second.length){
+        return null
+    }
+    let result=[]
+    for(let i=0;i<first.length;i++){
+        result.push(first[i])
+        result.push(second[i])
+    }
+
+return result
+
+}
+
+console.log(merge([1],[1,2]))
+console.log(merge([1,3],[2,4]))
